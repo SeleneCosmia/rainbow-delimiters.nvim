fffd11035a968211f9e85f58f1b5c316cadb7e7a
SeleneCosmia <hebe@selenocosmia.space>
2024-02-21 03:07:10 -0800

    fully flesh out elvish syntax

--- queries/elvish/rainbow-delimiters.scm
+++ queries/elvish/rainbow-delimiters.scm
@@ -6,6 +6,34 @@
   "{" @delimiter
   "}" @delimiter @sentinel) @container
 
+(elif
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(else
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(while
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(for
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(try
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(catch
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(finally
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
 (output_capture
   "(" @delimiter
   ")" @delimiter @sentinel) @container
